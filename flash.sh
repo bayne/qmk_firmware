0fd2c7847d4e122dcd33466c7556c27cca18b488
Brian Payne <bpayne@nagatha.home.southroute.com>
2025-02-15 17:32:27 -0800

    added nagatha keyboard config

--- flash.sh
+++ flash.sh
@@ -1,3 +1,5 @@
 # this is for ergodox
 /usr/local/bin/wally
 # for ploopy adept, unplug then replug while holding bottom left button. drag file into usb storage
+# for framework keyboard, remove touchpad, remove keyboard, press and hold both alt keys, reinstall keyboard and trackpad, release alt keys, open filemanager, mount rp drive
+# qmk flash -kb framework/ansi -km bayne
